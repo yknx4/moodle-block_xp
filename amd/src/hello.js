6b923b79e11d69b66fedc4ec2f25dd2a5cae7c01
Jorge <yknx4@live.com.mx>
2015-10-20 16:11:56 -0500

    fail2

--- amd/src/hello.js
+++ amd/src/hello.js
@@ -8,7 +8,7 @@
  /**
   * @module block_overview/helloworld
   */
-  define(['jquery'], function($) {
+  define(['jquery','toastr'], function($,toastr) {
 
      /** 
       * Give me blue.
@@ -53,27 +53,27 @@
 
 
     greeting.prototype.initialize = function ($params) {
-      alert('lalala');
-      // $.getScript( "toastr.js", function( toastr ) {
-      //   toastr.options = {
-      //     "closeButton": false,
-      //     "debug": false,
-      //     "newestOnTop": true,
-      //     "progressBar": false,
-      //     "positionClass": "toast-bottom-center",
-      //     "preventDuplicates": false,
-      //     "onclick": null,
-      //     "showDuration": "300",
-      //     "hideDuration": "1000",
-      //     "timeOut": "5000",
-      //     "extendedTimeOut": "1000",
-      //     "showEasing": "swing",
-      //     "hideEasing": "linear",
-      //     "showMethod": "fadeIn",
-      //     "hideMethod": "fadeOut"
-      //   }
-      //   toastr["info"]("You reacher n level.", "Level Up");
-      // });
+      
+      
+        toastr.options = {
+          "closeButton": false,
+          "debug": false,
+          "newestOnTop": true,
+          "progressBar": false,
+          "positionClass": "toast-bottom-center",
+          "preventDuplicates": false,
+          "onclick": null,
+          "showDuration": "300",
+          "hideDuration": "1000",
+          "timeOut": "5000",
+          "extendedTimeOut": "1000",
+          "showEasing": "swing",
+          "hideEasing": "linear",
+          "showMethod": "fadeIn",
+          "hideMethod": "fadeOut"
+        }
+        toastr["info"]("You reacher n level.", "Level Up");
+      
     };
 
 

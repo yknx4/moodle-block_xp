9c5bb10a4b3f190f2347882a9c73a1211383222f
Jorge <yknx4@live.com.mx>
2015-10-20 16:41:44 -0500

    nf1

--- hack.js
+++ hack.js
@@ -1,8 +1,23 @@
 requirejs(['jquery'], function( $ ) {
 	$.getScript("https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.js", function(toastr) {
-	  console.log( "Load was performed." );
-	  alert( "Load was performed." );
-
+		toastr.options = {
+			"closeButton": false,
+			"debug": false,
+			"newestOnTop": true,
+			"progressBar": false,
+			"positionClass": "toast-bottom-center",
+			"preventDuplicates": false,
+			"onclick": null,
+			"showDuration": "300",
+			"hideDuration": "1000",
+			"timeOut": "5000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+			"showMethod": "fadeIn",
+			"hideMethod": "fadeOut"
+		};
+		toastr["info"]("You've reached level 3", "Level Up!");
 	});
 
 

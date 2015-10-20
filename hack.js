a27ee1fb09b0308393ecfe06133ee84044390a47
Jorge <yknx4@live.com.mx>
2015-10-20 16:40:08 -0500

    ntf7

--- hack.js
+++ hack.js
@@ -1,7 +1,5 @@
 requirejs(['jquery'], function( $ ) {
-	$.getScript( "toastr.js", function(toastr) {
-
-
+	$.getScript("https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.js", function(toastr) {
 	  console.log( "Load was performed." );
 	  alert( "Load was performed." );
 

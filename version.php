8be42b02729b1568d6b42e65b68bfd74fe449fc6
Jorge <yknx4@live.com.mx>
2015-10-20 15:58:50 -0500

    Bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2015091102;
+$plugin->version    = 2015091103;
 $plugin->requires   = 2014041500;
 $plugin->component  = 'block_xp';
 $plugin->maturity   = MATURITY_BETA;
